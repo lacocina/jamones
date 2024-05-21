3e4ebb510f17e4c051a0abd1e60b13837b964fc3
Vicenç Álvarez Garau <31107136+lopollodesign@users.noreply.github.com>
2024-05-21 20:20:36 +0200

    Punto estable del llamado al endpoint de actualizar precio del jamón

--- front/src/components/ham/PriceBanner.vue
+++ front/src/components/ham/PriceBanner.vue
@@ -1,7 +1,7 @@
 <template>
   <the-banner title="Precio del jamón"
+              :button-text="buttonLabel"
               :price="priceLabel"
-              button-text="Cambiar precio del jamón"
               :disabled-message="disabledMessage"
               @click="openDialog">
     Puedes cambiar el precio del jamón en este pedido. Esto afectará a todos los cálculos en los pedidos de clientes.
@@ -12,6 +12,7 @@
 import { computed } from "vue";
 import { ClosedModal } from "../../types/ClosedModal.ts";
 import { useOverlay } from "@composables/useOverlay.ts";
+import { api } from "../../services/api.ts";
 
 import TheBanner from "@components/shared/TheBanner.vue";
 import PriceDialog from "@components/ham/PriceDialog.vue";
@@ -23,15 +24,29 @@ interface Props {
 
 const props = defineProps<Props>()
 
+interface Emits {
+  (ev: 'update-price', result: number): void
+}
+
+const emit = defineEmits<Emits>()
+
 const disabledMessage = computed(() => !!(props.price) ? '' : 'Aún no has añadido el precio del jamón.' )
 const priceLabel = computed(() => !!(props.price) ? `${props.price}€/kg` : '' )
+const buttonLabel = computed(() => !!(props.price) ? 'Cambiar' : 'Añadir' )
 
 const { open } = useOverlay()
 
 async function openDialog() {
   try {
     const response = await open(<PriceDialog price={props.price}/>)
-    console.log(response)
+    if (response.reason === 'confirm' && response.value) {
+      try {
+        const { data } = await api.patch(`packages/updateHamPrice/${props.packageId}`, { hamPrice: response.value })
+        emit('update-price', data.hamPrice)
+      } catch (e) {
+        console.error(e)
+      }
+    }
   } catch (e) {
     if (e !== ClosedModal) {
       console.log('Err')
