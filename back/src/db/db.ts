356c5b4a2bc57ac2243bd8f340d3c771a4b5e10e
Vicenç Álvarez Garau <31107136+lopollodesign@users.noreply.github.com>
2024-06-04 15:12:56 +0200

    Cambiamos la instancia de la bdd para que se inicie cada vez que se usa. Pendiente de cambiar.

--- back/src/db/db.ts
+++ back/src/db/db.ts
@@ -1,17 +1,13 @@
 import { Client } from "pg";
 
-const db = new Client("postgresql://lopollo@localhost:5432/jamones")
-
-let connected = false
-
 export async function getDBInstance() {
-    if (!connected) {
-        db.on("error", (err) => {
-            console.error('Error connecting to db', err.stack)
-            process.exit(-1)
-        })
-        await db.connect()
-        connected = true
-    }
-    return db
+    const client = new Client("postgresql://lopollo@localhost:5432/jamones")
+
+    client.on("error", (err) => {
+        console.error('Hello Error connecting to db', err.stack)
+        process.exit(-1)
+    })
+
+    await client.connect()
+    return client
 }
